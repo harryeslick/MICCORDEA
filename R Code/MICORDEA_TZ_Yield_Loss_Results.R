4757f3f6c40304b1a201bade2842a5a3eb378cc2
adamhsparks <adamhsparks@gmail.com>
2014-07-23 11:23:01 +0800

    Generate violin plots of yield losses
    Violin plots and eps of resulting plots

--- R Code/MICORDEA_TZ_Yield_Loss_Results.R
+++ R Code/MICORDEA_TZ_Yield_Loss_Results.R
@@ -14,6 +14,7 @@
 library(raster)
 library(ggplot2)
 library(reshape)
+library(extrafont)
 ##### End libraries ####
 
 #### Begin functions ####
@@ -28,77 +29,39 @@ tz.ya <- stack(list.files(path = "~/Google Drive/Data/MICORDEA/GPS3 Yields", pat
 #### End data import ####
 
 #### Begin data munging 
-## Calculate the percent losses
-tz.bb.loss <- (tz.ya-tz.bb)/tz.ya*100
-tz.lb.loss <- (tz.ya-tz.lb)/tz.ya*100
+## Calculate the yield losses
+tz.bb.loss <- (tz.ya-tz.bb)
+tz.lb.loss <- (tz.ya-tz.lb)
 
 ## Create data frames
 bb <- na.omit(data.frame(values(tz.bb.loss)))
 lb <- na.omit(data.frame(values(tz.lb.loss)))
 
-# A1B
-bb.a1b.2030 <- data.frame(bb[, 3], bb[, 7])
-names(bb.a1b.2030) <- c("A1B", "Base")
-bb.a1b.2030 <- melt(bb.a1b.2030)
+names(bb) <- names(lb) <- c("A2\n2030", "A2\n2050", "A1B\n2030", "A1B\n2050", "B1\n2030", "B1\n2050", "Base")
 
-bb.a1b.2050 <- data.frame(bb[, 4], bb[, 7])
-names(bb.a1b.2050) <- c("A1B", "Base")
-bb.a1b.2050 <- melt(bb.a1b.2050)
-
-# A2
-bb.a2.2030 <- data.frame(bb[, 1], bb[, 7])
-names(bb.a2.2030) <- c("A2", "Base")
-bb.a2.2030 <- melt(bb.a2.2030)
-
-bb.a2.2050 <- data.frame(bb[, 2], bb[, 7])
-names(bb.a2.2050) <- c("A2", "Base")
-bb.a2.2050 <- melt(bb.a2.2050)
-
-# B1
-bb.b1.2030 <- data.frame(bb[, 5], bb[, 7])
-names(bb.b1.2030) <- c("B1", "Base")
-bb.b1.2030 <- melt(bb.b1.2030)
-
-bb.b1.2050 <- data.frame(bb[, 6], bb[, 7])
-names(bb.b1.2050) <- c("B1", "Base")
-bb.b1.2050 <- melt(bb.b1.2050)
+bb_melted <- melt(bb)
+lb_melted <- melt(lb)
 
 #### Begin data visualisation ####
-## A1B 2030
-p.bb <- ggplot(bb.a1b.2030, aes(x = value, fill = variable, group = variable))
-p.bb1 <- p.bb + stat_density(alpha = 0.4) + 
-  labs(x = "", fill = "Scenario", colour = "Scenario", title = "2030")
+## Graphs of yield loss
+## BB
+p.bb <- ggplot(bb_melted, aes(x = variable, y = value))
+p.bb <- p.bb + geom_violin(aes(fill = variable, colour = variable)) +
+  labs(x = "Scenario and\nTimeslice", y = "Yield loss (tonnes/ha)") + theme(legend.position = "none")
 
-# A1B 2050
-p.bb <- ggplot(bb.a2.2050, aes(x = value, fill = variable))
-p.bb2 <- p.bb + stat_density(alpha = 0.4) + 
-  xlab("Percent yield loss due to Bacterial Blight") +
-  labs(x = "", fill = "Scenario", colour = "Scenario", title = "2050", y = "")
+ggsave(filename = "BB_Losses_Violin.eps", path = "Graphics", width = 140, height = 140, units = "mm")
 
-## A2 2030
-p.bb <- ggplot(bb.a2.2030, aes(x = value, fill = variable))
-p.bb3 <- p.bb + stat_density(alpha = 0.4) + 
-  xlab("Percent yield loss due\nto Bacterial Blight") +
-  labs(x = "", fill = "Scenario", colour = "Scenario")
+## LB
+p.lb <- ggplot(lb_melted, aes(x = variable, y = value))
+p.lb <- p.lb + geom_violin(aes(fill = variable, colour = variable)) +
+  labs(x = "Scenario and\nTimeslice", y = "Yield loss (tonnes/ha)") + theme(legend.position = "none") +
+  theme(axis.title.x = element_text(size = 10, family = "Helvetica"),
+        axis.title.y = element_text(size = 10, angle = 90, family = "Helvetica"),
+        axis.text = element_text(size = 9, family = "Helvetica"))
+ggsave(filename = "LB_Losses_Violin.eps", path = "Graphics", width = 140, height = 140, units = "mm")
 
-# A2 2050
-p.bb <- ggplot(bb.a1b.2050, aes(x = value, fill = variable))
-p.bb4 <- p.bb + stat_density(alpha = 0.4) + 
-  xlab("Percent yield loss due to Bacterial Blight") +
-  labs(x = "", fill = "Scenario", colour = "Scenario", y = "")
+## Maps of yield loss
 
-## B1
-p.bb <- ggplot(bb.b1.2030, aes(x = value, fill = variable))
-p.bb5 <- p.bb + stat_density(alpha = 0.4) + 
-  xlab("Percent yield loss due to Bacterial Blight") + 
-  labs(x = "Yield loss due\nto Bacterial Blight (%)", fill = "Scenario", colour = "Scenario")
-
-p.bb <- ggplot(bb.b1.2050, aes(x = value, fill = variable))
-p.bb6 <- p.bb + stat_density(alpha = 0.4) + 
-  xlab("Percent yield loss due to Bacterial Blight") +
-  labs(x = "Yield loss due\nto Bacterial Blight (%)", y = "", fill = "Scenario", colour = "Scenario")
-
-multiplot(p.bb1, p.bb3, p.bb5, p.bb2, p.bb4, p.bb6, cols = 2)
 
 #### End data visualisation ####
 
