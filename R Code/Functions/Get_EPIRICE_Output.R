d3f9f89972572694e8947a78f77e3894791020fd
Adam H. Sparks <adamhsparks@gmail.com>
2016-01-07 17:52:34 +0800

    New function to download EPIRCE Data from FigShare

---
+++ R Code/Functions/Get_EPIRICE_Output.R
@@ -0,0 +1,22 @@
+################################################################################
+# title         : Get_EPIRICE_Output.R;
+# purpose       : download EPIRICE disease severity files from Figshare;
+# producer      : prepared by A. Sparks;
+# last update   : in Los Baños, Laguna, Jan 2016;
+# inputs        : ;
+# outputs       : 2050, 2030 and base folders with EPIRICE data files;
+# remarks 1     : ;
+# Licence:      : GPL2;
+################################################################################
+
+download_EPIRICE_output <- function(){
+  tf <- tempfile()
+
+  if(length(list.files(path = "../Data/EPIRICE Output")) != 3){
+    download.file("https://ndownloader.figshare.com/files/3661902",
+                  tf, method = "wb")
+    unzip(tf, exdir = "../Data", overwrite = TRUE)
+  }
+}
+
+# eos
