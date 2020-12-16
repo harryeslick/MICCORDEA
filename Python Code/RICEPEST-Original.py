88198e9ae90a301b77c9b78674095b2cb3e77217
Adam H. Sparks <adamhsparks@gmail.com>
2020-12-17 08:04:05 +1000

    Close out open “(“ after Sander’s name

--- Python Code/RICEPEST-Original.py
+++ Python Code/RICEPEST-Original.py
@@ -1,7 +1,7 @@
 #NAME: RICEPEST Spatial Model
 #DESCRIPTION: Generic RICEPEST tool for calculating attainable and actual yields based on supplied parameters
 #REQUIREMENTS: ArcGIS Spatial Analyst Extension
-#DEVELOPED BY: Confidence Duku (AfricaRice), Adam Sparks (IRRI), Sander Zwart (AfricaRice
+#DEVELOPED BY: Confidence Duku (AfricaRice), Adam Sparks (IRRI), Sander Zwart (AfricaRice)
 #BASED ON WORK DONE BY: Laetitia Willocquet and Serge Savary (IRRI)
 
 #ORIGINAL VERSION WITH UNMODIFIED PS3
